303c3945606b979eb5f212bf619d0280ec8eea7d
Chan Wai Lou <sc18cwl@leeds.ac.uk>
2021-02-06 23:50:34 +0000

    Fix implicit module path

--- aumix/signal/fourier_series.py
+++ aumix/signal/fourier_series.py
@@ -9,14 +9,17 @@ Generates signals approximated with Fourier series.
 
 import numpy as np
 
-import simple_signal as ss
+import aumix.signal.simple_signal as ss
 
 
 
 class FourierSignal(ss.Signal):
+    """Abstract base class for Fourier Signals."""
     
     def __init__(self, n, **kwargs):
         self.n = n
+        self.ns = range(1, self.n+1)
+        
         super().__init__(**kwargs)
 
 
@@ -44,11 +47,10 @@ class FourierSawtoothSignal(FourierSignal):
     
     def gen_data(self):
         
-        ns = range(1, self.n+1)
         self.data = []
         
         for t in self.samp_nums:
-            sum_terms = [1/n * np.sin(2 * n * np.pi * self.freq * t) for n in ns]
+            sum_terms = [1/n * np.sin(2 * n * np.pi * self.freq * t) for n in self.ns]
     
             y = 1/2 - 1/np.pi * sum(sum_terms)
             self.data.append(y)
@@ -66,36 +68,35 @@ class FourierSeriesSignal(FourierSignal):
     
     """
     
-    def __init__(self, k, cos_coeffs, sin_coeffs, **kwargs):
+    def __init__(self, cos_coeffs, sin_coeffs, k=0, **kwargs):
         self.k = k
         self.cos_coeffs = cos_coeffs
         self.sin_coeffs = sin_coeffs
         super().__init__(**kwargs)
         
+        
+    def __cosine_sum(self):
+        cosine_components = [self.cos_coeffs[n-1] * np.cos(2 * np.pi * n * self.freq * self.samp_nums)
+                             for n in self.ns]
+        return np.sum(cosine_components, axis=0)
+        
+        
+    def __sine_sum(self):
+        sine_components = [self.sin_coeffs[n-1] * np.sin(2 * np.pi * n * self.freq * self.samp_nums)
+                           for n in self.ns]
+        return np.sum(sine_components, axis=0)
+        
     
     def gen_data(self):
         
-        ns = range(1, self.n+1)
-        
-        # cosines
-        cosine_components = [self.cos_coeffs[n-1] * np.cos(2 * np.pi * n * self.freq * self.samp_nums)
-                             for n in ns]
-        cosine_sum = np.sum(cosine_components, axis=0)
-        
-        # sines
-        sine_components = [self.sin_coeffs[n-1] * np.sin(2 * np.pi * n * self.freq * self.samp_nums)
-                           for n in ns]
-        sine_sum = np.sum(sine_components, axis=0)
-        
-        # add everything up
-        self.data = 0.5 * self.k + cosine_sum + sine_sum
+        # add constant, cosine components, and sine components together
+        self.data = 0.5 * self.k + self.__cosine_sum() + self.__sine_sum()
         
         if self.options.get("normalize", False):
             max_amp = max(self.data)
             self.data /= max_amp
         
         self.data = np.array(self.data)
-
         
         
        
