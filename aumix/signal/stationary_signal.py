ff76891c95d45b15d0c4e41c6badf8a4ba3ef613
VincentLou <sc18cwl@leeds.ac.uk>
2021-02-07 20:01:34 +0000

    Rename

--- aumix/signal/stationary_signal.py
+++ aumix/signal/stationary_signal.py
@@ -12,34 +12,35 @@ import numpy as np
 import aumix.signal.fourier_series as fs
 
 
-
-class PeriodicCompositeSignal(fs.FourierSeriesSignal):
+class StationarySignal(fs.FourierSeriesSignal):
     """
     A representation of a generic, non-piecewise-smooth signal of mulitple frequencies.
+    
+    Notice Fourier Series states that any such function can be represented
+    as a sum of sines and cosines, hence we don't need to specify any
+    coefficients other than for sin and cos.
     
     Parameters
     ----------
     See `FourierSeriesSignal`.
     """
-    
+
     def __init__(self, cos_freqs, sin_freqs, **kwargs):
         self.cos_freqs = cos_freqs
         self.sin_freqs = sin_freqs
         super().__init__(**kwargs)
-        
-        
+
+    # @overrides
     def __cosine_sum(self):
-        cosine_components = [self.cos_coeffs[n-1] * np.cos(2 * np.pi * n * self.cos_freqs[n-1] * self.samp_nums)
+        cosine_components = [self.cos_coeffs[n - 1] * np.cos(2 * np.pi * n * self.cos_freqs[n - 1] * self.samp_nums)
                              for n in self.ns]
         return np.sum(cosine_components, axis=0)
-        
-    
+
+    # @overrides
     def __sine_sum(self):
-        sine_components = [self.sin_coeffs[n-1] * np.cos(2 * np.pi * n * self.sin_freqs[n-1] * self.samp_nums)
+        sine_components = [self.sin_coeffs[n - 1] * np.cos(2 * np.pi * n * self.sin_freqs[n - 1] * self.samp_nums)
                            for n in self.ns]
         return np.sum(sine_components, axis=0)
-        
-        
 
 
 class ClarinetApproxSignal(fs.FourierSeriesSignal):
@@ -48,22 +49,21 @@ class ClarinetApproxSignal(fs.FourierSeriesSignal):
     
     TODO: Amplitude values taken from ...
     """
-    
+
     def __init__(self, **kwargs):
         super().__init__(n=0, cos_coeffs=None, sin_coeffs=None, **kwargs)
-        
-    
+
     def gen_data(self):
         odd_amplitudes = [1, 0.75, 0.50, 0.14, 0.50, 0.12, 0.17]
-        self.n = len(odd_amplitudes)*2
-        self.ns = range(1, self.n+1)
-        
+        self.n = len(odd_amplitudes) * 2
+        self.ns = range(1, self.n + 1)
+
         self.sin_coeffs = [0
-                           if i % 2 == 0 
-                           else 
-                           odd_amplitudes[int((i-1)/2)] 
-                           for i in range(1, self.n+1)]
+                           if i % 2 == 0
+                           else
+                           odd_amplitudes[int((i - 1) / 2)]
+                           for i in range(1, self.n + 1)]
         self.cos_coeffs = [0 for i in range(self.n)]
-        
+
         # Generate clarinet signal
         super().gen_data()
--- examples/stationary_signal_plot.py
+++ examples/stationary_signal_plot.py
@@ -5,18 +5,16 @@ periodic_composite_signal_plot.py
 @author: Chan Wai Lou / Vincent Lou
 """
 
-import aumix.signal.periodic_composite_signal as pcs
+import aumix.signal.stationary_signal as pcs
 import aumix.plot.plot as aplot
 
 import numpy as np
 
-
-    
 # parameters
 duration = 0.01
 samp_rate = 44100
 
-cl_f = 466.16 / 2   # written pitch C4, actual pitch is Bb4
+cl_f = 466.16 / 2  # written pitch C4, actual pitch is Bb4
 cl_pitch_label = "C4"
 
 signal_params = {
@@ -24,11 +22,9 @@ signal_params = {
     "samp_rate": samp_rate,
     "freq": cl_f,
     "options": {
-            "normalize": True
-        }
+        "normalize": True
+    }
 }
-
-
 
 # Generate clarinet signal using predefined amplitudes
 cl_signal = pcs.ClarinetApproxSignal(**signal_params)
@@ -40,8 +36,8 @@ general_options = {"xlabel": "time (seconds)",
 cl_line_options = [{"label": f"f0 = {cl_f}Hz"}]
 
 # Plot signals
-aplot.single_plot(cl_signal.samp_nums, 
-                  [cl_signal.data], 
+aplot.single_plot(cl_signal.samp_nums,
+                  [cl_signal.data],
                   line_options=cl_line_options,
                   title=f"Synthesized clarinet (written pitch {cl_pitch_label})",
                   **general_options)
