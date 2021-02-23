8cf8576d8c29512ea094888fbdf47d3a798ce2eb
VincentLou <sc18cwl@leeds.ac.uk>
2021-02-23 07:54:53 +0000

    Change single_plot and single_subplots to use FigData

--- aumix/plot/plot.py
+++ aumix/plot/plot.py
@@ -7,6 +7,8 @@ Plotting functions.
 @author: Chan Wai Lou / Vincent Lou
 """
 
+from aumix.plot.fig_data import *
+
 import matplotlib.pyplot as pl
 # from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -14,10 +16,11 @@ import numpy as np
 
 # ------------------------- Decorators --------------------------- #
 
+
 def savefig(function):
     """
     Save the figure in drawn function if savefig_path is specified.
-    
+
     Intended to be used as a decorator.
 
     Parameters
@@ -44,14 +47,7 @@ def savefig(function):
 
 
 @savefig
-def single_plot(xs: np.ndarray,
-                ys: list,
-                line_options=[],
-                title="",
-                xlabel="",
-                ylabel="",
-                figsize=(9, 7),
-                options=[],
+def single_plot(fig_data: FigData = None,
                 **kwargs
                 ):
     """
@@ -59,45 +55,9 @@ def single_plot(xs: np.ndarray,
 
     Parameters
     ----------
-    xs : np.ndarray
-        Data points in the x-axis. 
-        For example, np.array([0, 0.1, 0.2, 0.3, 0.4]).
-        
-    ys : list
-        A list of data points stored in `np.ndarray`s.
-        Each `np.ndarray` contains elements y_i 
-        that is plotted against x_i in xs.
-        
-    line_options : list, default: []
-        A list each containing a dictionary for options to draw each line.
-        Not all lines need to be supplied an option, i.e. the length of
-        line_options does not need to be the same as ys.
-        
-        For example, if we have two lines, we can create line_options as so:
-            line_options[0] = {
-                    label="Line 1"
-                }
-            line_options[1] = {
-                    label="Line 2",
-                    linewidth=2.5,
-                    color="black"
-                }
-        
-    title : str, default: ""
-        The title of the plot.
-        
-    xlabel : str, default: ""
-        The x-axis label.
-        
-    ylabel : str, default: ""
-        The y-axis label.
-        
-    figsize : tuple, default: (9, 7)
-        Figure size of the plot.
+    fig_data : FigData
+        Encapsulated figure data.
 
-    options : list, default: []
-        "grid": display a grid behind the plot.
-        
     **kwargs : dict
         Other keyward arguments.
 
@@ -106,79 +66,99 @@ def single_plot(xs: np.ndarray,
     None.
 
     """
-    # Fill up line_options if it's not fully specified
-    num_lines = len(ys)
-    num_options = len(line_options)
-    __line_options = [{} for line in range(num_lines)]
 
-    # Specified in line_options, use it
-    for line_idx in range(num_options):
-        __line_options[line_idx] = line_options[line_idx]
+    # Don't plot anything if fig_data isn't specified
+    if fig_data is None:
+        return
 
     # Create the figure
-    fig = pl.figure(figsize=figsize)
+    fig = pl.figure(figsize=fig_data.figsize)
     ax = fig.add_subplot(111)
-    ax.set_title(title)
-    ax.set_ylabel(ylabel)
-    ax.set_xlabel(xlabel)
+    ax.set_title(fig_data.title)
+    ax.set_ylabel(fig_data.ylabel)
+    ax.set_xlabel(fig_data.xlabel)
 
-    if "grid" in options:
+    if "grid" in fig_data.options:
         ax.grid()
 
     # Plot all ys
-    for i, line in enumerate(ys):
+    for i, line in enumerate(fig_data.ys):
         line_len = len(line)
-        ax.plot(xs[:line_len], line, **__line_options[i])
+        ax.plot(fig_data.xs[:line_len], line, **fig_data.line_options[i])
 
     ax.legend()
 
 
 @savefig
-def single_subplots(xs={},
-                    ys={},
+def single_subplots(fig_data: dict = None,
                     n_rows=1,
                     n_cols=1,
-                    individual_figsize=(4, 4),
-                    line_options={},
-                    titles={},
-                    xlabels={},
-                    ylabels={},
-                    options=[],
+                    individual_figsize=(6, 6),
+                    title="",
                     **kwargs
                     ):
+    """
+    Plot one figure with n_rows-by-n_cols subplots.
+
+    Parameters
+    ----------
+    fig_data: dict, optional
+        A dictionary of (subplot_pos, figure_data).
+        subplot_pos expects a 3-tuple to be used in fig.add_subuplot.
+            For example, (1, 2, 1) means the first subplot with a whole figure having 1 row and 2 columns.
+        figure_data expects a FigData object, encapsulating the figure's data.
+
+        Justification: Using a dict rather than a list with each FigData having a "subplot_pos" keyword variable
+        is more explicit, and so is preferred.
+
+    n_rows: int, default: 1
+        Number of rows of figures.
+
+    n_cols: int, default: 1
+        Number of columns of figures.
+
+    individual_figsize: tuple, default: (6, 6)
+        Size of each individual subplot.
+        Note: the figsize tuple value in each FigData object in fig_data is ignored.
+
+    title: string, default: ""
+        Title of the master figure.
+
+    kwargs: dict
+        Other keyword arguments.
+
+    Returns
+    -------
+
+    """
+
+    # Don't plot anything if fig_data isn't specified, or contains no figure data
+    if fig_data is None or len(fig_data) == 0:
+        return
 
     # Create figure template
     fig = pl.figure(figsize=(individual_figsize[0] * n_rows, individual_figsize[1] * n_cols))
+    pl.title(title)
     fig.subplots_adjust(left=-0.2, right=1, top=1, bottom=-0.2)
 
     # Find figure positions where both the x-axis and y-axis have data
-    fig_pos = set(xs.keys()) & set(ys.keys())   # Python 2 friendly
 
-    for key in fig_pos:
-        (row, col) = key
-
-        # Fill up line_options if it's not fully specified
-        num_lines = len(ys[key])    # ys must contain the key since it's intersected
-        num_options = len(line_options.get(key, {}))
-        __line_options = [{} for line in range(num_lines)]
-
-        # Specified in line_options, use it
-        for line_idx in range(num_options):
-            __line_options[line_idx] = line_options[line_idx]
+    for ((row, col, num), f) in fig_data.items():
 
         # Create the specified subplot
-        ax = fig.add_subplot(n_rows, n_cols, row * n_cols + col + 1)
-        ax.set_title(titles.get(key, ""))
-        ax.set_ylabel(ylabels.get(key, ""))
-        ax.set_xlabel(xlabels.get(key, ""))
+        # ax = fig.add_subplot(n_rows, n_cols, row * n_cols + col + 1)
+        ax = fig.add_subplot(row, col, num)
+        ax.set_title(f.title)
+        ax.set_ylabel(f.ylabel)
+        ax.set_xlabel(f.xlabel)
 
-        if "grid" in options:
+        if "grid" in f.options:
             ax.grid()
 
         # Plot all ys
-        for i, line in enumerate(ys[key]):
+        for i, line in enumerate(f.ys):
             line_len = len(line)
-            ax.plot(xs[key][:line_len], line, **__line_options[i])
+            ax.plot(f.xs[:line_len], line, **f.line_options[i])
 
         ax.legend()
 
@@ -195,17 +175,17 @@ def small_plot(xs: np.ndarray,
     Parameters
     ----------
     xs : np.ndarray
-        Data points in the x-axis. 
+        Data points in the x-axis.
         For example, np.array([0, 0.1, 0.2, 0.3, 0.4]).
-        
+
     ys : list
         A list of data points stored in `np.ndarray`s.
-        Each `np.ndarray` contains elements y_i 
+        Each `np.ndarray` contains elements y_i
         that is plotted against x_i in xs.
-        
+
     figsize : tuple, default: (9, 7)
         Figure size of the plot.
-        
+
     **kwargs : dict
         Other keyward arguments.
 
