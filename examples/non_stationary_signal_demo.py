c8a5b0b3e9fbe4d34ac761be5c8502deaac562ac
VincentLou <sc18cwl@leeds.ac.uk>
2021-03-13 07:19:34 +0000

    Include a non-stationary signal with chopped sub-signals

--- examples/non_stationary_signal_demo.py
+++ examples/non_stationary_signal_demo.py
@@ -16,9 +16,12 @@ from aumix.plot.fig_data import *
 # Parameters
 #
 n_signals = 4
-freqs = range(20, 20 + 20 * n_signals, 20)
-durations = [max(1 - i * 0.25, 0.25) for i in range(n_signals)]
-chop_ranges = [None for i in range(n_signals)]
+freqs = range(1, n_signals+1)
+durations = [1] * n_signals
+chop_ranges = [(0, 0.5)] * n_signals
+# freqs = range(20, 20 + 20 * n_signals, 20)
+# durations = [max(1 - i * 0.25, 0.25) for i in range(n_signals)]
+# chop_ranges = [None] * n_signals
 
 #
 # Generate data
