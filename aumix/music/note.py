2a53e8060586006a8f192aea1cb4932d58da6bc6
VincentLou <sc18cwl@leeds.ac.uk>
2021-03-17 22:35:17 +0000

    Letter note to frequency. Fix #21

--- aumix/music/note.py
+++ aumix/music/note.py
@@ -5,8 +5,20 @@ Musical notes.
 
 """
 
+notes = [
+    "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
+    "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
+    "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+    "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+    "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+    "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
+    "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
+    "C8", "C#8", "D8", "D#8", "E8", "F8", "F#8", "G8", "G#8", "A8", "A#8", "B8",
+    "C9", "C#9", "D9", "D#9", "E9", "F9", "F#9", "G9", "G#9", "A9", "A#9", "B9"]
 
-def note2freq(f0, octave, n):
+
+def note2freq(n, octave=0, f0=440):
     """
     Compute the freq of a note using the equal tempered scale.
 
@@ -15,7 +27,7 @@ def note2freq(f0, octave, n):
 
     Parameters
     ----------
-    f0: float
+    f0: float, default = 440
         Fundamental freq. Commonly this is chosen to be the A above middle C (A4),
         at f0 = 440 Hz.
 
@@ -37,6 +49,27 @@ def note2freq(f0, octave, n):
     """
 
     return f0 * 2 ** ((12 * octave + n) / 12)
+
+
+def notename2freq(name, a4=440):
+    """
+    Returns the frequency of a letter note.
+
+    Parameters
+    ----------
+    name: str
+        Letter note.
+
+    a4: int, default = 440
+        Frequency of A4.
+
+    Returns
+    -------
+    Frequency of the specified letter note.
+
+    """
+    half_steps = notes.index(name.upper()) - notes.index("A4")
+    return note2freq(n=half_steps, octave=0, f0=a4)
 
 
 def freq2note(fn, a4=440, ret_cents=False):
@@ -70,20 +103,6 @@ def freq2note(fn, a4=440, ret_cents=False):
 
     if (fn < 27.5) or (fn > 14080):
         raise ValueError("Frequency must lie between 27.5Hz (A0) and 14080Hz (A9)")
-
-    a4_index = 57
-
-    notes = [
-        "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
-        "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
-        "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
-        "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
-        "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
-        "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
-        "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
-        "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
-        "C8", "C#8", "D8", "D#8", "E8", "F8", "F#8", "G8", "G#8", "A8", "A#8", "B8",
-        "C9", "C#9", "D9", "D#9", "E9", "F9", "F#9", "G9", "G#9", "A9", "A#9", "B9"]
 
     r = 2 ** (1 / 12)
     cent = 2 ** (1 / 1200)
@@ -123,4 +142,5 @@ def freq2note(fn, a4=440, ret_cents=False):
         else:
             side = -1 if cent_index != 0 else 1
 
-    return (notes[a4_index + r_index], side * cent_index) if ret_cents else notes[a4_index + r_index]
+    return (notes[notes.index("A4") + r_index], side * cent_index) if ret_cents else notes[
+        a4_index + r_index]
