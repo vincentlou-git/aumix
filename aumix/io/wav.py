aa9a12de276e96b48ad2dba0919dae8440aca08d
VincentLou <sc18cwl@leeds.ac.uk>
2021-03-27 08:25:09 +0000

    .wav convertor

---
+++ aumix/io/wav.py
@@ -0,0 +1,60 @@
+"""
+wav.py
+
+Module for converting to .wav files from signal data.
+
+@author: Chan Wai Lou / Vincent Lou
+"""
+
+import numpy as np
+from scipy.io import wavfile
+import os
+
+
+def signal2wav(filename, signal, samp_rate=None, dtype=np.int32):
+    """
+
+    Parameters
+    ----------
+    filename
+    signal
+    samp_rate
+    dtype
+        np.uint8 : 8-bit PCM
+        np.int16 : 16-bit PCM
+        np.int32 : 32-bit PCM
+        np.float32 : 32-bit floating point
+
+    Returns
+    -------
+
+    """
+
+    data = signal
+
+    # If signal is a Signal class, then retrieve data and sampling rate from it
+    if "data" in vars(signal):
+        data = signal.data
+        samp_rate = signal.samp_rate
+
+    # If sampling rate is undefined, we don't have enough info to output a .wav file
+    if samp_rate is None:
+        raise ValueError("Sampling rate is undefined.")
+
+    # Find out the maximum size of the specified type
+    amplitude = np.iinfo(dtype).max if dtype != np.float32 else 1
+
+    # Treat 8-bit PCM as a special case, since the numbers are unsigned.
+    if dtype == np.uint8:
+        out_data = data * amplitude / max(data)
+    else:
+        unsigned_data = data - min(data)
+        out_data = unsigned_data * amplitude / max(unsigned_data)
+
+    # Create folder if it doesn't exist
+    folder = "/".join(filename.split("/")[:-1])
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # Output file
+    wavfile.write(f"{filename}.wav", samp_rate, out_data.astype(dtype))
--- aumix/io/wav_gen.py
+++
@@ -1,8 +0,0 @@
-"""
-wav_gen.py
-
-Module for generating .wav files from signal data.
-
-@author: Chan Wai Lou / Vincent Lou
-"""
-
