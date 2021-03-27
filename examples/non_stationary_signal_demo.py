af1e6dad4f437eb3a95e35aff45306efb0f98885
VincentLou <sc18cwl@leeds.ac.uk>
2021-03-27 18:14:12 +0000

    Export .wav file

--- examples/non_stationary_signal_demo.py
+++ examples/non_stationary_signal_demo.py
@@ -8,6 +8,7 @@ Example of using the NonStationarySignal class.
 
 import aumix.signal.simple_signal as ss
 import aumix.signal.non_stationary_signal as nsts
+from aumix.io.wav import *
 import aumix.plot.plot as aplot
 from aumix.plot.fig_data import *
 
@@ -16,12 +17,12 @@ from aumix.plot.fig_data import *
 # Parameters
 #
 n_signals = 4
-freqs = range(1, n_signals+1)
+# freqs = range(1, n_signals+1)
 durations = [1] * n_signals
-chop_ranges = [(0, 0.5)] * n_signals
-# freqs = range(20, 20 + 20 * n_signals, 20)
+# chop_ranges = [(0, 0.5)] * n_signals
+freqs = range(220, 220 + 220 * n_signals, 220)
 # durations = [max(1 - i * 0.25, 0.25) for i in range(n_signals)]
-# chop_ranges = [None] * n_signals
+chop_ranges = [None] * n_signals
 
 #
 # Generate data
@@ -45,3 +46,4 @@ fig = FigData(xs=nst.samp_nums,
 # Plot
 #
 aplot.single_plot(fig)
+signal2wav("audio/nonst8", nst, dtype=np.uint8)
