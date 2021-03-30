aea3250b571fe5154427abe521f46649bd3677cc
VincentLou <sc18cwl@leeds.ac.uk>
2021-03-30 06:57:23 +0100

    Module for major scale frequencies generation

---
+++ aumix/music/major_scale.py
@@ -0,0 +1,21 @@
+"""
+major_scale.py
+
+Module for generating major scales.
+
+@author: DoraMemo
+"""
+
+import aumix.music.note as note
+
+import numpy as np
+
+semitone_steps = [2, 2, 1, 2, 2, 2, 1]
+
+
+def maj_freqs(scale_name, n_notes=8):
+
+    n_octaves = int(np.ceil(n_notes / 8))
+
+    fund_freq = note.notename2freq(scale_name)   # Root note for the scale
+    return np.array([note.note2freq(sum((semitone_steps*n_octaves)[:i]), f0=fund_freq) for i in range(n_notes)])
