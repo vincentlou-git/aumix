7d7c9855e21325aa6bd2e313bb158a4130b60c3a
VincentLou <sc18cwl@leeds.ac.uk>
2021-04-04 19:21:25 +0100

    Demonstration of ISTFT. Fix #31

--- examples/inverse_stft.py
+++ examples/inverse_stft.py
@@ -6,9 +6,11 @@ Inverse short-time Fourier transform attempt.
 @author: Chan Wai Lou
 """
 
+import copy
 from scipy import signal
+import music21 as m21
 
-import aumix.music.major as maj
+import aumix.music.notes as notes
 import aumix.signal.stationary_signal as sts
 import aumix.signal.non_stationary_signal as nsts
 from aumix.io.wav import *
@@ -18,38 +20,63 @@ from aumix.plot.fig_data import *
 #
 # Parameters
 #
+# Musical parameters
 scale_name = "A4"
-n_notes = 4
+extra_notes = ["C6", "E6", "A4", "C5"]
+n_chords = 5
+
+# Filtering parameters
+freq_absence_tol = 5e-2
+lopass_threshold = m21.note.Note("B5").pitch.frequency
+hipass_threshold = m21.note.Note("D5").pitch.frequency
+
+# Signal parameters
 samp_rate = 44100
-freq_absence_tol = 5e-3
-filter_threshold = 3100   # Hz
+norm_amp = [[1.5] * 4] * (n_chords - 1) + [[2] * 3]
+durations = [1] * n_chords
+chop_ranges = [None] * n_chords
 
-freqs = maj.scale_freqs(scale_name=scale_name, n_notes=n_notes * 2)
-freqs = freqs[::2]   # Create arpeggio
-durations = [0.25] * n_notes
-chop_ranges = [None] * n_notes
-
-window = "blackmanharris"
+# STFT parameters
+window = "hann"
+window_length = 8192
+overlap_percent = 0.5
 
 #
 # Generate data
 #
 
-half_duration = sum(durations) / 2
+cutoff_duration = sum(durations) * 0.4
+
+# A4 C5 E5 for "A4"
+chord = notes.minor_chord(scale_name=scale_name, n_notes=3, output="chord")
+
+melody = m21.stream.Stream()
+for i in range(1, n_chords+1):
+    inv = i % 3
+    melody.append(chord)
+    chord_copy = copy.deepcopy(chord)
+    chord_copy.inversion(inv)   # Move root note to the top
+    chord = chord_copy
+
+# Add the extra notes to each chord - we want to remove these after STFT
+for i, name in enumerate(extra_notes):
+    melody[i].add(name)
 
 # Generate signal
-signals = [sts.ClarinetApproxSignal(freq=freqs[i],
-                                    duration=durations[i],
-                                    samp_rate=samp_rate,
-                                    chop_range=chop_ranges[i])
-           for i in range(len(freqs))]
+signals = [sts.StationarySignal(sin_freqs=[p.frequency for p in chord.pitches],
+                                sin_coeffs=norm_amp[i],
+                                duration=durations[i],
+                                samp_rate=samp_rate,
+                                chop_range=chop_ranges[i])
+           for i, chord in enumerate(melody)]
 nst = nsts.NonStationarySignal(signals)
 
 # Compute STFT
 f, t, Zxx = signal.stft(x=nst.data,
                         fs=nst.samp_rate,
                         window=window,
-                        nperseg=1024)
+                        nperseg=window_length,
+                        noverlap=int(window_length * overlap_percent))
 
 Zxx_max = np.max(np.abs(Zxx))
 
@@ -58,14 +85,15 @@ Zxx_max = np.max(np.abs(Zxx))
 # This cannot be done with a single FFT & IFFT since such a threshold function
 # is non-linear, hence STFT (or other methods, such as Butterworth's filter) must be used.
 
-freqoff = np.where(f >= filter_threshold)[0][0]
-toff = np.where(t >= half_duration)[0][0]
+looff = np.where(f <= lopass_threshold)[0][-1]
+hioff = np.where(f >= hipass_threshold)[0][0]
+toff = np.where(t >= cutoff_duration)[0][0]
 Zxxf = np.zeros(Zxx.shape, dtype=Zxx.dtype)
 
-low = Zxx[:freqoff, :toff]
-high = Zxx[freqoff:, toff:]
-Zxxf[:freqoff, :toff] = Zxx[:freqoff, :toff]
-Zxxf[freqoff:, toff:] = Zxx[freqoff:, toff:]
+low = Zxx[:looff, :toff]
+high = Zxx[hioff:, toff:]
+Zxxf[:looff, :toff] = low
+Zxxf[hioff:, toff:] = high
 
 
 # Compute Inverse STFT
@@ -75,9 +103,17 @@ trec, xrec = signal.istft(Zxxf, samp_rate)
 #
 # Encapsulate data
 #
+
+# Zoom: Find max / min frequency present in the signal (less than some tolerance)
+cond = [all(x < freq_absence_tol) for x in Zxx]
+min_freq_idx = cond.index(False) - 1
+cond.reverse()
+max_freq_idx = len(cond) - cond.index(False) - 1
+ylim = (f[min_freq_idx], f[max_freq_idx]*1.1)
+
 signal_fig = FigData(xs=nst.samp_nums,
                      ys=nst.data,
-                     title=f"{scale_name} Major scale arpeggio (Artificial clarinet sound)",
+                     title=f"'Contaminated' {scale_name} minor chord (Pure sine waves)",
                      plot_type="plot",
                      xlabel="Time (s)",
                      ylabel="Amplitude")
@@ -85,7 +121,9 @@ signal_fig = FigData(xs=nst.samp_nums,
 stft_fig = FigData(xs=t,
                    ys=f,
                    zs=np.abs(Zxx),
-                   title=f"STFT Magnitude",
+                   title=f"STFT Magnitude w/ {window} window\n"
+                         f"Window length = {window_length}\n"
+                         f"Overlap = {overlap_percent*100}%",
                    line_options=[{"vmin": 0,
                                   "vmax": Zxx_max,
                                   "shading": 'gouraud'}],
@@ -93,12 +131,17 @@ stft_fig = FigData(xs=t,
                    plot_type="pcolormesh",
                    xlabel="Time (s)",
                    ylabel="Frequency (Hz)",
+                   ylim=ylim,
+                   yscale="log",
                    fit_data=False)
 
 stftf_fig = FigData(xs=t,
                     ys=f,
                     zs=np.abs(Zxxf),
-                    title=f"Filtered STFT: Lowpass on first half; highpass on second half\nThreshold at {filter_threshold}Hz",
+                    title=f"Filtered STFT\n"
+                          f"Cutoff duration: {cutoff_duration}s\n"
+                          f"Before cutoff: Lowpass at {'{:.2f}'.format(lopass_threshold)}Hz\n"
+                          f"After cutoff: Highpass at {'{:.2f}'.format(hipass_threshold)}Hz",
                     line_options=[{"vmin": 0,
                                    "vmax": Zxx_max,
                                    "shading": 'gouraud'}],
@@ -106,11 +149,14 @@ stftf_fig = FigData(xs=t,
                     plot_type="pcolormesh",
                     xlabel="Time (s)",
                     ylabel="Frequency (Hz)",
+                    ylim=ylim,
+                    yscale="log",
                     fit_data=False)
 
 recon_fig = FigData(xs=trec,
                     ys=xrec,
-                    title="Reconstructed after filtering",
+                    title="Reconstructed after filtering:\n"
+                          f"{scale_name} minor triads w/ inversions",
                     plot_type="plot",
                     xlabel="Time (s)",
                     ylabel="Amplitude")
@@ -125,7 +171,7 @@ aplot.single_subplots(grid_size=(2, 2),
                                 (0, 0): signal_fig,
                                 (1, 0): recon_fig},
                       individual_figsize=(6, 4),
-                      # savefig_path=f"ISTFT_Clarinet_{scale_name}_Major_scale_arpeggio_sampr={samp_rate}"
+                      savefig_path=f"ISTFT_{window}_{window_length}_{overlap_percent*100}%_Pure_{scale_name}_minor_chord_sampr={samp_rate}"
                       )
 
-signal2wav(f"audio/istft_cl_{scale_name}_Major_arpeggio", np.concatenate((nst.data, xrec)), samp_rate=samp_rate)
+signal2wav(f"audio/ISTFT_{window}_{window_length}_{overlap_percent*100}%_Pure_{scale_name}m", np.concatenate((nst.data, xrec)), samp_rate=samp_rate)
