1dd0e01712d365265dd5ccfc5e45c72bdfa29cb2
VincentLou <sc18cwl@leeds.ac.uk>
2021-05-19 00:38:06 +0100

    Refinement

--- examples/ft_chord_melody.py
+++ examples/ft_chord_melody.py
@@ -13,6 +13,7 @@ import aumix.signal.non_stationary_signal as nsts
 import aumix.plot.plot as aplot
 from aumix.plot.fig_data import *
 import aumix.music.notes as anotes
+import aumix.io.wav as wav
 
 import numpy as np
 from scipy.fft import fft, fftfreq
@@ -23,15 +24,17 @@ from scipy.fft import fft, fftfreq
 scale_name = "C2"
 n_notes = 3
 samp_rate = 44100
-durations = [0.25] * n_notes
+durations = [1] * n_notes
 
 #
 # Generate signals
 #
-freqs = anotes.major_scale(scale_name=scale_name, n_notes=n_notes, output="freq")
+freqs = anotes.major_chord(scale_name=scale_name, n_notes=n_notes, output="freq")
 note_names = " ".join([n.nameWithOctave for n in
-                       anotes.major_scale(scale_name=scale_name, n_notes=n_notes,
+                       anotes.major_chord(scale_name=scale_name, n_notes=n_notes,
                                           output="note")])
+underline_note_names = note_names.replace(" ", "_")
+duration = sum(durations)
 
 signals = [ss.SineSignal(freq=freqs[i],
                          duration=durations[i],
@@ -40,7 +43,7 @@ signals = [ss.SineSignal(freq=freqs[i],
 melody = nsts.NonStationarySignal(signals)
 chord = sts.StationarySignal(sin_freqs=freqs,
                              sin_coeffs=[1] * n_notes,
-                             duration=sum(durations),
+                             duration=duration,
                              samp_rate=samp_rate)
 
 # Only consider positive frequencies (since the spectrum is symmetric)
@@ -56,25 +59,30 @@ chord_fft = fft(chord.data)[:slice_num] / (chord.duration * samp_rate)
 # Figures
 #
 
+label = ord('a')
+
 sig_figs = [None, None]
 fft_figs = [None, None]
 
-for i, (sig, ft, fft_x, name) in enumerate([(melody, melody_fft, melody_fft_x, "Melody"),
-                                            (chord, chord_fft, chord_fft_x, "Chord")]):
+for i, (sig, ft, fft_x, name) in enumerate([(chord, chord_fft, chord_fft_x, "Chord"),
+                                            (melody, melody_fft, melody_fft_x, "Melody")]):
     # Encapsulate signals data
     sig_figs[i] = FigData(xs=sig.samp_nums,
                           ys=[sig.data],
-                          title=f"{name} of {note_names}",
+                          title=f"({chr(label)}) {name} of {note_names}",
                           plot_type="plot",
                           xlabel="Time (s)",
                           ylabel="Amplitude")
+    label += 1
 
     fft_figs[i] = FigData(xs=fft_x[:slice_num],
                           ys=np.abs(ft) ** 2,
-                          title=f"Fourier Transform Magnitude of {name}",
+                          title=f"({chr(label)}) Fourier Transform Magnitude of {name}",
                           options=["grid"],
                           xlabel="Frequency (Hz)",
                           ylabel="Normalised Magnitude")
+
+    label += 1
 
 # Plot
 aplot.single_subplots(grid_size=(2, 2),
@@ -84,5 +92,9 @@ aplot.single_subplots(grid_size=(2, 2),
                                 (1, 1): fft_figs[1]},
                       individual_figsize=(5, 3),
                       auto_timestamp=True,
-                      savefig_path=f"{name}_{scale_name}_FFT"
+                      savefig_path=f"{underline_note_names}_{round(duration*1000)}ms_{name}_FFT.png"
                       )
+
+# Output wav
+wav.write(f"audio/{underline_note_names}_{round(duration*1000)}ms_melody", melody)
+wav.write(f"audio/{underline_note_names}_{round(duration*1000)}ms_chord", chord)
