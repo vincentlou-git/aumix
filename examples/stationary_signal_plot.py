95a33c1e52f95dd4666cf44ec5d885333a832af8
VincentLou <sc18cwl@leeds.ac.uk>
2021-05-26 20:20:53 +0100

    Update to use FigData

--- examples/stationary_signal_plot.py
+++ examples/stationary_signal_plot.py
@@ -7,6 +7,7 @@ periodic_composite_signal_plot.py
 
 import aumix.signal.stationary_signal as pcs
 import aumix.plot.plot as aplot
+from aumix.plot.fig_data import *
 
 import numpy as np
 
@@ -30,14 +31,14 @@ signal_params = {
 cl_signal = pcs.ClarinetApproxSignal(**signal_params)
 
 # Figure options
-general_options = {"xlabel": "time (seconds)",
-                   "ylabel": "Amplitude"}
-
-cl_line_options = [{"label": f"f0 = {cl_f}Hz"}]
+cl_fig = FigData(xs=cl_signal.samp_nums,
+                 ys=[cl_signal.data],
+                 line_options=[{"label": f"f0 = {cl_f}Hz"}],
+                 xlabel="Time (seconds",
+                 ylabel="Amplitude",
+                 title=f"Synthesized clarinet (written pitch {cl_pitch_label})",
+                 figsize=(5, 3)
+                 )
 
 # Plot signals
-aplot.single_plot(cl_signal.samp_nums,
-                  [cl_signal.data],
-                  line_options=cl_line_options,
-                  title=f"Synthesized clarinet (written pitch {cl_pitch_label})",
-                  **general_options)
+aplot.single_plot(cl_fig)
